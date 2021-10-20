db9ccd82939cc84ba7d4c9b8da3b106eb8d857ea
unknown <crutoi2@yahoo.com>
2021-10-20 10:06:24 +0300

    Update

--- stack/stack/stack.h
+++ stack/stack/stack.h
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #ifndef __stack
 #define __stack
+
+#include <stdio.h>
+#include <stdlib.h>
 
 #define TRUE  1;
 #define FALSE 0;
@@ -12,11 +12,11 @@ typedef struct stack{
     int *data;
 }t_stack;
 
-void INIT_STACK(t_stack *, int);
-void PUSH(t_stack *, int);
-void PRINTSTACK(t_stack);
-int  STACK_EMPTY(t_stack);
-int  POP(t_stack *);
-void FREE_STACK(t_stack *);
+void INIT_STACK(t_stack *S, int init_size);
+void PUSH(t_stack *S, int value);
+void PRINTSTACK(t_stack S);
+int  STACK_EMPTY(t_stack S);
+int  POP(t_stack *S);
+void FREE_STACK(t_stack *S);
 
 #endif 
